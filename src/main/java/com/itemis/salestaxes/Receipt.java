fe420fe482280b55403992980c3ec5f9e5a7d2da
Mouad <mouad10wolf@gmail.com>
2022-11-10 19:45:40 +0100

    Commenting Receipt class

--- src/main/java/com/itemis/salestaxes/Receipt.java
+++ src/main/java/com/itemis/salestaxes/Receipt.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- *
+ * Represents receipt calculated
+ * 
  * @author Mouad Douieb
  */
 public class Receipt {
@@ -15,18 +16,27 @@ public class Receipt {
         getOrder();
     }
 
+    /**
+    * Get user input from console and process it
+    *
+    */
     public void getOrder() {
 
+        // Scan user inputs
         Scanner sc = new Scanner(System.in);
         System.out.println("### INPUT:");
+
         try {
             while (sc.hasNextLine()) {
 
                 String line = sc.nextLine();
 
+                // Break the loop if the user entered an empty line
                 if (line.isEmpty()) {
                     break;
                 } else {
+                    
+                    // Adding new order to the list
                     order.add(line);
                 }
 
@@ -38,11 +48,14 @@ public class Receipt {
         }
     }
 
+    /**
+    * Print Receipt cart list, Sales Tax and Total
+    *
+    */
     public void printReceipt() {
         
         for (String orderItem : order) {
             System.out.printf(orderItem + "\n");
-
         }
 
         System.out.printf("\nSales Tax: %.2f \n", 0.00);
