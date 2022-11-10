6288af7a44c3006c02c77596b2e176b120f0c149
Mouad <mouad10wolf@gmail.com>
2022-11-10 19:21:02 +0100

    Add Receipt and Main Classes

---
+++ src/main/java/com/itemis/salestaxes/Main.java
@@ -0,0 +1,12 @@
+package com.itemis.salestaxes;
+
+/**
+ *
+ * @author Mouad Douieb
+ */
+public class Main {
+    public static void main(String[] args) {
+        Receipt receipt = new Receipt();
+        receipt.printReceipt();
+    }
+}
---
+++ src/main/java/com/itemis/salestaxes/Receipt.java
@@ -0,0 +1,27 @@
+package com.itemis.salestaxes;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mouad Douieb
+ */
+public class Receipt {
+
+    ArrayList<String> order = new ArrayList<String>();
+
+    public Receipt() {
+        getOrder();
+    }
+
+    public void getOrder() {
+
+        
+    }
+
+    public void printReceipt() {
+        
+        
+    }
+}
