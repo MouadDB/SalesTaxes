6e1d85032069cb926d68ae40cbcd4e0174d97a6f
Mouad <mouad10wolf@gmail.com>
2022-11-10 20:10:10 +0100

    Reafactor Item class

--- src/main/java/com/itemis/salestaxes/Item.java
+++ src/main/java/com/itemis/salestaxes/Item.java
@@ -17,6 +17,7 @@ class Item {
      */
     private static final double INTIAL_IMPORT_DUTY_RATE = 0.05;
 
+    
     /**
      * Product name
      */
@@ -40,8 +41,24 @@ class Item {
     /**
      * Is the product taxable ?
      */
-    private boolean taxable;
+    private boolean taxable = true;
 
+    /**
+     * Default Constructor
+     */
+    public Item() {
+        this.name = "";
+        this.price = 0.00;
+    }
+
+    /**
+     * Item parameterized constructor
+     * 
+     * @param name
+     * @param price
+     * @param quantity
+     * @param taxable 
+     */
     public Item(String name, double price, int quantity, boolean taxable) {
         this.name = name;
         this.price = price;
@@ -49,52 +66,113 @@ class Item {
         this.taxable = taxable;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public void setImported(boolean imported) {
-        this.imported = imported;
-    }
-
-    public void setTaxable(boolean taxable) {
-        this.taxable = taxable;
-    }
-
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get the value of price
+     *
+     * @return the value of price
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Set the value of price
+     *
+     * @param price new value of price
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * Get the value of quantity
+     *
+     * @return the value of quantity
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Set the value of quantity
+     *
+     * @param quantity new value of quantity
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * Get the value of imported
+     *
+     * @return the value of imported
+     */
     public boolean isImported() {
         return imported;
     }
 
+    /**
+     * Set the value of imported
+     *
+     * @param imported new value of imported
+     */
+    public void setImported(boolean imported) {
+        this.imported = imported;
+    }
+
+    /**
+     * Get the value of taxable
+     *
+     * @return the value of taxable
+     */
     public boolean isTaxable() {
         return taxable;
     }
 
-    public static double getINTIAL_TAX_RATE_FOR_GOODS() {
-        return INTIAL_TAX_RATE_FOR_GOODS;
+    /**
+     * Set the value of taxable
+     *
+     * @param taxable new value of taxable
+     */
+    public void setTaxable(boolean taxable) {
+        this.taxable = taxable;
     }
 
+
+    /**
+     * Get the value of INTIAL_IMPORT_DUTY_RATE
+     *
+     * @return the value of INTIAL_IMPORT_DUTY_RATE
+     */
     public static double getINTIAL_IMPORT_DUTY_RATE() {
         return INTIAL_IMPORT_DUTY_RATE;
+    }
+
+    /**
+     * Get the value of INTIAL_TAX_RATE_FOR_GOODS
+     *
+     * @return the value of INTIAL_TAX_RATE_FOR_GOODS
+     */
+    public static double getINTIAL_TAX_RATE_FOR_GOODS() {
+        return INTIAL_TAX_RATE_FOR_GOODS;
     }
 
 }
