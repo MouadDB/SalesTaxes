3dabd2fdd51169dd13c589095759cf7b0eea21e0
Mouad <mouad10wolf@gmail.com>
2022-11-10 21:05:32 +0100

    Add Others class

---
+++ src/main/java/com/itemis/salestaxes/Others.java
@@ -0,0 +1,21 @@
+package com.itemis.salestaxes;
+
+/**
+ *
+ * @author Mouad Douieb
+ */
+public class Others extends Item {
+    
+    /**
+     * Food parameterized constructor
+     * 
+     * @param name
+     * @param price
+     * @param quantity
+     * @param isImported 
+     */
+    public Others(String name, double price, int quantity,  boolean isImported) {
+        super(name, price, quantity, isImported);
+    }
+
+}
