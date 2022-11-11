488a9c3c942deef8b3c2a636ac7d7d76088879fc
Mouad <mouad10wolf@gmail.com>
2022-11-11 20:47:46 +0100

    Add roundToFive and round methods

--- src/main/java/com/itemis/salestaxes/Utils.java
+++ src/main/java/com/itemis/salestaxes/Utils.java
@@ -14,4 +14,36 @@ public class Utils {
         }
         return "others";
     }
+
+    public static double round(double value, int places) {
+        if (places < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
+
+    public static double roundToFive(double input) {
+        int cents = (int) (input * 100);
+        int lastDigit = cents % 10;
+        boolean mustAddUnit = false;
+        if (lastDigit > 5) {
+            mustAddUnit = true;
+            lastDigit = 0;
+        } else if (lastDigit > 0) {
+            lastDigit = 5;
+        } else {
+            lastDigit = 0;
+        }
+        cents = cents / 10;
+        if (mustAddUnit) {
+            cents += 1;
+        }
+        cents = cents * 10;
+        cents = cents + lastDigit;
+        return cents / 100F;
+    }
 }
